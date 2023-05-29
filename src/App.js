535f59013ce1abbd2a84a5483ef653e001000d3a
Danish <danishsidd524@gmail.com>
2023-05-30 01:11:21 +0500

    Sppech To Text Convertor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from "react-use-clipboard";
 
 function App() {
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-US' })
+
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <>
+      <h2 className='error'>this browser doesnt support speech recognition. Please try with another browser instead</h2>
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <h2>Speech To Text Convertor</h2>
+      
+      <div className="main">
+
+        <div className="main-content">
+            {transcript}
+        </div>
+
+        <div className="btns">
+          <button className="btn" onClick={startListening}>Start Listening</button>
+          <button className="btn" onClick={() => SpeechRecognition.stopListening()}>Stop Listening</button>
+        </div>
+        
+      </div>
+
     </div>
+    </>
   );
 }
 
